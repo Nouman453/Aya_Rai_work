ab6edc335a449cdeac8680a5862ed888c3eefa4e
Nouman Ali <133819437+Nouman453@users.noreply.github.com>
2023-11-03 22:04:01 +0200

    Add files via upload

---
+++ Ay.py
@@ -0,0 +1,55 @@
+# a=1
+# b=3
+# print(a+b)
+
+# s=input("Enter your first input")
+# v=input("Enter your second input")
+# print(s+v)
+
+# driver= int(input("Enter the age of driver"))
+# if driver>=18:
+#     print("you can drive")
+# else:
+#     print("you can't drive") 
+   
+# import os
+# os.system("C:/Users/nouma/Desktop/vv/test.mp3")
+
+# import time
+# start=time.time()
+# print("This is printed immediately.")
+# time.sleep(3)
+# print("This is printed after a 2-second delay.")
+# end=time.time()
+# print(end-start)
+
+# a= "AYa"
+# print(len(a))
+
+# mylist=[1,"Aya",2,3,4,5,6,7]
+# print(mylist)
+# mylist.append("Rai")
+# print(mylist)
+
+# Aya=int(input(go))
+# if Aya==9:
+#     print("Aya can enter")
+# elif aya==10:
+#     print("Aya can enter another class")
+# else: 
+#     print("Aya cannot enter")
+
+# thelist=["Mohamed,Rawda,Rai"]
+# print(thelist)
+# thelist.append("Aya")
+# print(thelist)
+
+# thelist=["Mohamed,Rawda,Rai","Aya"]
+# print(thelist)
+# thelist.remove("Aya")
+# print(thelist)
+    
+
+
+    
+    
